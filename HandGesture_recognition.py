a4d1fe9ff09d84996102ba97fea9891db4e65ee6
Guiyin TIAN <u3610546@connect.hku.hk>
2024-04-18 23:22:10 +0800

    delete old data set

--- HandGesture_recognition.py
+++ HandGesture_recognition.py
@@ -14,22 +14,3 @@ class LeNet5(nn.Module):
 
 
 
-def dataset():
-    train_data_folder = './data/train_set'
-    test_data_folder = './data/test_set'
-
-    transform = transforms.Compose([
-                transforms.Grayscale(),
-                transforms.RandomRotation(15),
-                transforms.Resize((28, 28)),
-                transforms.ToTensor(),
-                transforms.Normalize((0.1307,), (0.3081,)
-                )])
-    train_data = datasets.ImageFolder (root = train_data_folder, transform = transform)
-    test_data = datasets.ImageFolder (root = test_data_folder, transform = transform)
-
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size = batch_size, shuffle = True)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size = batch_size, shuffle = True)
-    
-    print ('Data loaded successfully')
-    return train_loader, test_loader
