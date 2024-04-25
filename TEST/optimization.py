b9904219c42ca356b7733fce644303412021e1b5
Bowie <aoibosh@connect.hku.hk>
2024-04-25 20:00:15 +0800

    update

--- TEST/optimization.py
+++ TEST/optimization.py
@@ -1,21 +1,16 @@
 from torchvision.models import resnet18
+from torchvision.models.resnet import ResNet18_Weights
 import torch.nn.utils.prune as prune
 import torch
 import torch.nn as nn
 
-class ResNet18(nn.Module):
-    def __init__(self):
-        super(ResNet18, self).__init__()
-        self.model = resnet18(pretrained=True) 
-        num_ftrs = self.model.fc.in_features
-        self.model.fc = nn.Linear(num_ftrs, 3) 
-
-    def forward(self, x):
-        return self.model(x)
 
 
-model = ResNet18()
-model.load_state_dict(torch.load('ResNet18_optimized.pth'))
+
+model = resnet18(pretrained = True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 3)  
+model.load_state_dict(torch.load('Trained_Models_test/ResNet18_Marked_96.pth'))
 model.eval()
 
 
@@ -23,13 +18,15 @@ conv_layers = [module for module in model.modules() if isinstance(module, nn.Con
 fc_layers = [module for module in model.modules() if isinstance(module, nn.Linear)]
 
 for layer in conv_layers:
-    prune.l1_unstructured(layer, name="weight", amount=0.2)
+    prune.l1_unstructured(layer, name="weight", amount=0.3)
 
 for layer in fc_layers:
-    prune.l1_unstructured(layer, name="weight", amount=0.2)
+    prune.l1_unstructured(layer, name="weight", amount=0.3)
 
 for layer in conv_layers:
     prune.remove(layer, "weight")
 
 for layer in fc_layers:
     prune.remove(layer, "weight")
+
+print("OK")
